148b9ec44ddd7750a8bfe7dc403bc7bdd36f04cc
RRRRRRRRRRRRR <s41268@bj.ac.th>
2025-12-18 12:33:30 +0700

    Add files via upload

---
+++ Midterm_13_14/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1766035614,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/coin-sheet0.png",
+		"images/coinbox-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/tiledbackground.png",
+		"images/background-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
